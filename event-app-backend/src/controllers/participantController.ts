571f507838d6ba4b8190f75bf52ace91dcba5076
Alain Bonneau <alaindev97@gmail.com>
2025-02-19 03:55:58 +0100

    add get participant by event controller

--- event-app-backend/src/controllers/participantController.ts
+++ event-app-backend/src/controllers/participantController.ts
@@ -17,6 +17,17 @@ export const registerToEvent = async (
       return;
     }
 
+    const participantCount = await Participant.count({ where: { eventId } });
+
+    if (participantCount >= event.maxParticipants) {
+      res
+        .status(403)
+        .json({
+          message: "L'événement a atteint son nombre maximum de participants",
+        });
+      return;
+    }
+
     const userAlreadyParticipant = await Participant.findOne({
       where: { userId, eventId },
     });
@@ -69,6 +80,39 @@ export const deleteParticipant = async (
   } catch (error) {
     res.status(500).json({
       message: "Erreur lors de la désinscription de l'événement",
+      error,
+    });
+  }
+};
+
+export const getParticipantByEvent = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const eventId = parseInt(req.params.id, 10);
+
+    if (isNaN(eventId)) {
+      res.status(400).json({ message: "ID invalide" });
+      return;
+    }
+
+    const event = await Event.findByPk(eventId);
+
+    if (!event) {
+      res.status(404).json({ message: "Evénement introuvable" });
+      return;
+    }
+
+    const allParticipants = await Participant.findAll({
+      where: { eventId },
+      include: [{ model: User, attributes: ["id", "email"] }],
+    });
+
+    res.status(200).json({ participants: allParticipants });
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de la récupération des participants",
       error,
     });
   }

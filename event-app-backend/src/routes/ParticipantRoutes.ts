6e2c614387365265044221aedb2b32444b38a996
Alain Bonneau <alaindev97@gmail.com>
2025-02-19 03:58:23 +0100

    add register to event and delete participant routes

--- event-app-backend/src/routes/ParticipantRoutes.ts
+++ event-app-backend/src/routes/ParticipantRoutes.ts
@@ -1,8 +1,14 @@
 import express from "express";
-import { registerToEvent } from "../controllers/participantController";
+import {
+  registerToEvent,
+  deleteParticipant,
+  getParticipantByEvent,
+} from "../controllers/participantController";
 
 const router = express.Router();
 
+router.get("/:id", getParticipantByEvent);
 router.post("/", registerToEvent);
+router.delete("/:id", deleteParticipant);
 
 export default router;

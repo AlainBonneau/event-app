ec0dbbc42ace3996f07876f1f090ad385deb7498
Alain Bonneau <alaindev97@gmail.com>
2025-02-24 22:13:39 +0100

    add events card componant

---
+++ event-app-frontend/src/components/EventCard.tsx
@@ -0,0 +1,27 @@
+interface Event {
+  id: number;
+  title: string;
+  description: string;
+}
+
+interface EventCardProps {
+  events: Event[];
+}
+
+const EventCard = ({ events }: EventCardProps) => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Tous les événements</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {events.map((event: Event) => (
+          <div key={event.id} className="border p-4">
+            <h2 className="text-xl font-bold mb-2">{event.title}</h2>
+            <p>{event.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
--- event-app-frontend/src/pages/Home.tsx
+++ event-app-frontend/src/pages/Home.tsx
@@ -1,11 +1,6 @@
 import { useState, useEffect } from "react";
 import api from "../api/axiosConfig";
-
-interface Event {
-  id: number;
-  title: string;
-  description: string;
-}
+import EventCard from "../components/EventCard";
 
 const Home = () => {
   const [events, setEvents] = useState([]);
@@ -31,16 +26,9 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Tous les événements</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {events.map((event: Event) => (
-          <div key={event.id} className="border p-4">
-            <h2 className="text-xl font-bold mb-2">{event.title}</h2>
-            <p>{event.description}</p>
-          </div>
-        ))}
-      </div>
+    <div className="home-page">
+      {/* Créer une navbar responsive avec tous ce qu'il faut */}
+      <EventCard events={events} />
     </div>
   );
 };

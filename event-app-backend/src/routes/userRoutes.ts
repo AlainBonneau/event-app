b44e52395bc9b38fcc48af78da4db12c8e506868
Alain Bonneau <alaindev97@gmail.com>
2025-02-24 02:11:00 +0100

    add update user route

--- event-app-backend/src/routes/userRoutes.ts
+++ event-app-backend/src/routes/userRoutes.ts
@@ -3,14 +3,16 @@ import {
   registerUser,
   loginUser,
   getUserProfile,
-  getCurrentUser
+  getCurrentUser,
+  updateProfile,
 } from "../controllers/userController";
 import { isAuthenticated } from "../middlewares/isAuthenticated";
 
 const router = express.Router();
 
 router.get("/profile/:id", getUserProfile);
-router.get("/me", isAuthenticated, getCurrentUser)
+router.get("/me", isAuthenticated, getCurrentUser);
+router.put("/me/update", isAuthenticated, updateProfile);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 

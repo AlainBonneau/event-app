209f65af4e0293e080d3ec39241e7e1c19571a95
Alain Bonneau <alaindev97@gmail.com>
2025-02-24 21:36:03 +0100

    add auth context

---
+++ event-app-frontend/src/context/AuthContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useState, ReactNode } from "react";
+
+type AuthContextType = {
+  token: string | null;
+  login: (token: string) => void;
+  logout: () => void;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("token")
+  );
+
+  const login = (newToken: string) => {
+    setToken(newToken);
+    localStorage.setItem("token", newToken);
+  };
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};

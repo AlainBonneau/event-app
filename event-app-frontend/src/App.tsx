f73fc9f0d8fd67bc8d73f1d620863623cfed2c0a
Alain Bonneau <alaindev97@gmail.com>
2025-02-26 01:36:00 +0100

    add responsive navbar and handle connexion

--- event-app-frontend/src/App.tsx
+++ event-app-frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import MyNavbar from "./components/MyNavbar";
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter } from "react-router-dom";
 
 const App = () => {
   return (
     <AuthProvider>
-      <MyNavbar />
-      <AppRoutes />
+      <BrowserRouter>
+        <MyNavbar />
+        <AppRoutes />
+      </BrowserRouter>
     </AuthProvider>
   );
 };
--- event-app-frontend/src/routes/AppRoutes.tsx
+++ event-app-frontend/src/routes/AppRoutes.tsx
@@ -1,19 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 // Importer les pages une fois qu'elles sont créées
 import Home from "../pages/Home";
 
 const AppRoutes = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/events" element={<Events />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      {/* <Route path="/events" element={<Events />} />
         <Route path="/events/:id" element={<Event />} />
         <Route path="/events/:id/edit" element={<EventEdit />} />
         <Route path="/events/new" element={<EventNew />} />
         <Route path="*" element={<NotFound />} /> */}
-      </Routes>
-    </Router>
+    </Routes>
   );
 };
 

bcd51aa276dc81c1fabdac5cb7dfec81d3a2991a
Alain Bonneau <alaindev97@gmail.com>
2025-02-26 01:09:47 +0100

    add Navbar component

--- event-app-frontend/src/App.tsx
+++ event-app-frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import MyNavbar from "./components/MyNavbar";
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <AuthProvider>
+      <MyNavbar />
       <AppRoutes />
     </AuthProvider>
   );

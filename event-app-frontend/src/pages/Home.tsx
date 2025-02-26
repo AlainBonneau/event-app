c7afe2d7d20fe051bd08ca0073c648717d2e864d
Alain Bonneau <alaindev97@gmail.com>
2025-02-26 22:46:37 +0100

    correct tailwind variable config

--- event-app-frontend/src/pages/Home.tsx
+++ event-app-frontend/src/pages/Home.tsx
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
     <div className="home-page">
-      <h1 className="text-3xl text-center text-test-100">Bienvenue sur Event'Go</h1>
-      <p className="text-center text-test-100">
+      <h1 className="text-3xl text-center">Bienvenue sur Event'Go</h1>
+      <p className="text-center">
         Le site pour trouver les événements qui vous correspondent
       </p>
     </div>

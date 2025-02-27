37a8a9fcfd8da45be0c2b5d20d0ae82308acc749
Alain Bonneau <alaindev97@gmail.com>
2025-02-28 00:59:55 +0100

    add login page

--- event-app-frontend/src/routes/AppRoutes.tsx
+++ event-app-frontend/src/routes/AppRoutes.tsx
@@ -2,14 +2,15 @@ import { Route, Routes } from "react-router";
 // Importer les pages une fois qu'elles sont créées
 import Home from "../pages/Home";
 import EventPage from "../pages/Event";
+import Login from "../pages/Login";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/events" element={<EventPage />} />
-      {/* <Route path="/events" element={<Events />} />
-        <Route path="/events/:id" element={<Event />} />
+      <Route path="/login" element={<Login />} />
+      {/* <Route path="/events/:id" element={<Event />} />
         <Route path="/events/:id/edit" element={<EventEdit />} />
         <Route path="/events/new" element={<EventNew />} />
         <Route path="*" element={<NotFound />} /> */}

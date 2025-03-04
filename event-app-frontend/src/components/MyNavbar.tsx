a8d55c3d90aba95a12bc3ab32c4aa2c3373940e7
Alain Bonneau <alaindev97@gmail.com>
2025-03-04 03:38:29 +0100

    add user picture in navbar

--- event-app-frontend/src/components/MyNavbar.tsx
+++ event-app-frontend/src/components/MyNavbar.tsx
@@ -84,7 +84,7 @@ export default function MyNavbar() {
                     <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-none">
                       <img
                         alt="Profil"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src=".\images\user.png"
                         className="size-8 rounded-full"
                       />
                     </MenuButton>

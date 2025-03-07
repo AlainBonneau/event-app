595f951be7988d17131980802b00dc5e9a3cfcbe
Alain Bonneau <alaindev97@gmail.com>
2025-03-08 00:09:27 +0100

    add new event button

--- event-app-frontend/src/pages/Event.tsx
+++ event-app-frontend/src/pages/Event.tsx
@@ -1,8 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import api from "../api/axiosConfig";
 import EventCard from "../components/EventCard";
+import { useNavigate } from "react-router";
+import { AuthContext } from "../context/AuthContext";
 
 const EventPage = () => {
+  const navigate = useNavigate();
+  const auth = useContext(AuthContext);
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
@@ -28,6 +32,16 @@ const EventPage = () => {
   return (
     <div className="EventPage-page">
       <h1 className="pt-18 text-center text-4xl">Événements</h1>
+    {auth?.token && (
+              <div className="btn-container flex items-center justify-end pr-4">
+              <button
+                className="px-6 py-3 bg-primary text-white font-semibold rounded-lg hover:bg-blue-600 transition-transform transform hover:scale-105 shadow-lg "
+                onClick={() => navigate("/create-event")}
+              >
+                +
+              </button>
+            </div>
+    )}
       <EventCard events={events} />
     </div>
   );

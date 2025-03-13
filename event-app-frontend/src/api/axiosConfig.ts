03c1ebbea6884e785fea58aadd7b7f9fa94326ce
Alain Bonneau <alaindev97@gmail.com>
2025-03-13 05:19:50 +0100

    change backend link

--- event-app-frontend/src/api/axiosConfig.ts
+++ event-app-frontend/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://147.93.55.31:5002/api",
+  baseURL: "https://event-api.sparcky-dev.fr",
   headers: {
     "Content-Type": "application/json",
   },

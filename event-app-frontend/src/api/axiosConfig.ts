575fbdba973a5b83dbdd7401793471f9cdb05d65
Alain Bonneau <alaindev97@gmail.com>
2025-03-13 05:25:13 +0100

    correct api link

--- event-app-frontend/src/api/axiosConfig.ts
+++ event-app-frontend/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://event-api.sparcky-dev.fr",
+  baseURL: "https://event-api.sparcky-dev.fr/api",
   headers: {
     "Content-Type": "application/json",
   },

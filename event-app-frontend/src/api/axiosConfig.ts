321c5e3c47ccc222b08d041b2a146d8d5e284b3a
Alain Bonneau <alaindev97@gmail.com>
2025-03-24 03:34:17 +0100

    add new api link

--- event-app-frontend/src/api/axiosConfig.ts
+++ event-app-frontend/src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: "https://147.93.55.31:5002/api",
   headers: {
     "Content-Type": "application/json",
   },

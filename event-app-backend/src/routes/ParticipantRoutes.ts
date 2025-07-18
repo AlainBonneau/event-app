b11c5de7bcec49adc5dca0ce286ab2e446f5fe1b
AlainDev <145831756+AlainBonneau@users.noreply.github.com>
2025-07-18 10:37:30 +0200

    Fix participants count route

--- event-app-backend/src/routes/ParticipantRoutes.ts
+++ event-app-backend/src/routes/ParticipantRoutes.ts
@@ -11,8 +11,8 @@ import {
 const router = express.Router();
 
 router.get("/check", checkParticipation);
-router.get("/:id", getParticipantByEvent);
 router.get("/count/:eventId", countParticipantsByEvent);
+router.get("/:id", getParticipantByEvent);
 router.post("/", registerToEvent);
 router.delete("/:id", deleteParticipant);
 router.delete("/", unregisterFromEvent);
